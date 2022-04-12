fc20213e45386ea9608684aeca64bd3146e0f203
S <gunass@deloitte.com>
2022-04-12 17:26:22 +0530

    selection and validation

--- mini_assignment_2/integration/select.spec.js
+++ mini_assignment_2/integration/select.spec.js
@@ -2,24 +2,32 @@
 
 describe('select validation', function()
 {
-
+    var data
     before(function(){
-        cy.fixture('example').then(function(data){
-            this.data=data
+        cy.fixture('example').then(function(inputdata){
+            data=inputdata
         })
     })
-    it('validation of selecting country', function()
+
+
+    it('Validation for selecting language', function()
     {
         cy.visit('http://demo.automationtesting.in/Register.html')
-        cy.get('[class="select2-selection select2-selection--single"]').type(this.data.country)
-        cy.get('#select2-country-results').each(($country,index)=>{
-            const countryText=$country.text()
-            if(countryText.includes(this.data.country)){
-                cy.get('#select2-country-results').eq(index).click()
+        cy.get('#msdd').click()
+        cy.get('ul[style="list-style:none;max-height: 230px;overflow: scroll;"] > li').each(($language,index)=>{
+            const languageText = $language.text()
+            if(languageText.includes(data.language1))
+            {
+                cy.get('ul[style="list-style:none;max-height: 230px;overflow: scroll;"] > li').eq(index).click()
+            }
+            if(languageText.includes(data.language2))
+            {
+                cy.get('ul[style="list-style:none;max-height: 230px;overflow: scroll;"] > li').eq(index).click()
             }
         })
-        cy.get('[class="select2-selection select2-selection--single"]').should('contain',this.data.country)
+        cy.get('#msdd').should('contain.text',data.language1,data.language2)
     })
+
 
     it('skills validation', function()
     {
@@ -27,21 +35,18 @@ describe('select validation', function()
         cy.get('#Skills').select('Data Analytics').should('have.value','Data Analytics')
     })
 
-    it('Validation for selecting language', function()
+
+    it('validation of selecting country', function()
     {
         cy.visit('http://demo.automationtesting.in/Register.html')
-        cy.get('#msdd').click()
-        cy.get('ul[class="ui-autocomplete ui-front ui-menu ui-widget ui-widget-content ui-corner-all"] > li').each(($lan1,index)=>{
-            const languageText = $lan1.text()
-            if(languageText.includes('English'))
-            {
-                cy.get('ul[class="ui-autocomplete ui-front ui-menu ui-widget ui-widget-content ui-corner-all"] > li').eq(index).click()
-            }
-            if(languageText.includes('Arabic'))
-            {
-                cy.get('ul[class="ui-autocomplete ui-front ui-menu ui-widget ui-widget-content ui-corner-all"] > li').eq(index).click()
+        cy.get('[class="select2-selection select2-selection--single"]').type(data.country)
+        cy.get('#select2-country-results').each(($country,index)=>{
+            const countryText=$country.text()
+            if(countryText.includes(data.country)){
+                cy.get('#select2-country-results').eq(index).click()
             }
         })
-        cy.get('#msdd').should('contain','English','Arabic')
+        cy.get('[class="select2-selection select2-selection--single"]').should('contain',data.country)
     })
+
 })
